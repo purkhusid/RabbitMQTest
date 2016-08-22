47d1d74677337695b32dbbb3731a898ea1824243
Daníel P. Purkhús <danielp@motus.is>
2016-08-22 14:10:21 +0000

    Initial commit

--- RabbitMQTest/Program.cs
+++ RabbitMQTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RabbitMQ.Client;
 
 namespace RabbitMQTest
 {
@@ -10,6 +11,26 @@ namespace RabbitMQTest
     {
         static void Main(string[] args)
         {
+            var connectionFactory = new RabbitMQ.Client.ConnectionFactory()
+            {
+                Password = "guest",
+                UserName = "guest",
+                HostName = "localhost"
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+
+            model.QueueDeclare("MyQueue", true, false, false, null);
+            Console.WriteLine("Queue created");
+
+            model.ExchangeDeclare("MyExchange", ExchangeType.Topic);
+            Console.WriteLine("Exchange created");
+
+            model.QueueBind("MyQueue", "MyExchange", "cars");
+            Console.WriteLine("My Queue bound to MyExchange");
+
+            Console.ReadLine();
         }
     }
 }
