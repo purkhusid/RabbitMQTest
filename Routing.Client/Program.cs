4a03e6e9bdd2eb9e19bd6142796ee88155461256
Daníel P. Purkhús <danielp@motus.is>
2016-08-23 15:49:07 +0000

    Routing demo

---
+++ Routing.Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Routing.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting RabbitMQ Message Sender");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var messageCount = 0;
+            var rabbitService = new RabbitService();
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Q)
+                    break;
+
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    var routingKey = messageCount%5 == 0 ? "route1" : "route2";
+                    var message = $"Message: {messageCount}";
+                    Console.WriteLine($"Sending - {message}");
+                    rabbitService.Send(message, routingKey);
+                    messageCount++;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Routing.Client/RabbitService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Routing.Client
+{
+    public class RabbitService : IDisposable
+    {
+        private const string HostName = "localhost";
+        private const string Username = "guest";
+        private const string Password = "guest";
+        private const string ExchangeName = "RoutingDemo"; //Fanout exchange
+
+        private readonly IConnection connection;
+        private readonly IModel channel;
+
+        public RabbitService()
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = HostName,
+                UserName = Username,
+                Password = Password
+            };
+
+            connection = connectionFactory.CreateConnection();
+            channel = connection.CreateModel();
+            channel.ExchangeDeclare(ExchangeName, "direct", true);
+        }
+
+        public void Send(string message, string routingKey)
+        {
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
+            var messageBuffer = Encoding.Default.GetBytes(message);
+
+            //We are using a direct exchange now.
+            //Queues specify what routing key they are interested in
+            channel.BasicPublish(ExchangeName, routingKey, properties, messageBuffer);
+        }
+
+        public void Dispose()
+        {
+            connection.Dispose();
+            channel.Dispose();
+        }
+    }
+}
